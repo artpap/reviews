5f716629c08f06bd22ea0384fa99c84d1c89fc04
artpap <100701975+artpap@users.noreply.github.com>
2023-03-23 19:27:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  return <h2>Reviews Starter</h2>;
+};
+export default App;
