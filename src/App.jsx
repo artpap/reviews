e66dd9fb868e595b44b7c8db8bf009984e9640bb
artpap <100701975+artpap@users.noreply.github.com>
2023-03-23 20:08:01 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,52 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  return <h2>Reviews Starter</h2>;
+
+  const nextPerson = () => {
+    let newIndex = index + 1 < people.length ? index + 1 : 0;
+    setIndex(newIndex);
+  };
+
+  const prevPerson = () => {
+    let newIndex = index - 1 >= 0 ? index - 1 : people.length - 1;
+    setIndex(newIndex);
+  };
+
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      //if equal to current index, go to the next
+      nextPerson();
+    } else {
+      setIndex(randomIndex);
+    }
+  };
+
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} alt={name} className='person-img' />
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn' onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className='btn btn-hipster' onClick={randomPerson}>
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
